c43da79e620f94d59960ca0897679f067f908932
Casper Beyer <caspervonb@pm.me>
2021-01-13 17:03:27 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ueo pipefail
+
+src_dir="src"
+out_dir="out"
+wasm_cc="/opt/wasi-sdk/bin/clang"
+
+mkdir -p "$out_dir"
+
+for input in "$src_dir"/*; do
+  echo "Copying $input"
+  cp $input "$out_dir/$(basename $input)"
+done
+
+for input in "$out_dir"/*.c; do
+  echo "Compiling $input"
+  $wasm_cc -o "$out_dir/$(basename $input .c).wasm" "$input"
+done
