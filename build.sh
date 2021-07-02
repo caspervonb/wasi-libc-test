c1312f4fda3d5a1c87add718d4bfdf524bece842
Casper Beyer <caspervonb@pm.me>
2021-07-02 05:05:30 +0000

    Take CC from the environment

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ueo pipefail
 
-wasm_cc="/opt/wasi-sdk/bin/clang"
+CC=${CC:=clang}
 
 for input in *.c; do
   echo "Compiling $input"
 
   output="$(basename $input .c).wasm"
-  $wasm_cc -o "$output" "$input"
+  $CC -o "$output" "$input"
 done
